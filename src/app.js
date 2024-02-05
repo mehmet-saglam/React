cfe351969681ace14d73737a30c6f4f2b8689784
Mehmet Saglam <159064949+mehmet-saglam@users.noreply.github.com>
2024-02-06 00:16:11 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,12 +8,18 @@
             alert("Butona tiklandi");
         }
 
+        // var value=null;
+        // function clickMe() {                    comment lines for writing output to the screen without alert box.
+        //     value =  "Butona tiklandi";
+        //     renderApp();
+        // }
 
         function renderApp() {
             var template2 = 
         (
         <div>
             <button onClick={clickMe}> Click me </button>
+           // {value != "" ? <h1>{value}</h1> : ""}  
         </div>
         );
         root.render(template2);
